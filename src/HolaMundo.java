50c5b43c563127440a9b4646a84087eeca5e5daa
Borja Fornas <bfornaschiva@gmail.com>
2022-10-27 18:40:43 +0200

    Initial commit

---
+++ src/HolaMundo.java
@@ -0,0 +1,9 @@
+import javax.swing.*;
+
+import static javax.swing.JOptionPane.INFORMATION_MESSAGE;
+
+public class HolaMundo {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Your name is Borja", "Info", INFORMATION_MESSAGE);
+    }
+}
